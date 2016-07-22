923a90448c594185c8205e7a191e4c49ac98e2e3
kongzichixiangjiao <58895347@qq.com>
2016-07-22 13:28:39 +0800

    链式编程例子

--- 链式编程/ViewController.swift
+++ 链式编程/ViewController.swift
@@ -9,17 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let chained = Chained()
+        
+        chained.show { (message) in
+            print(message)
+        }.add {
+            message in
+            print(message)
+        }.hide {
+            message in
+            print(message)
+        }.remove(false) {
+            [weak self] message in
+            if let weakSelf = self {
+                weakSelf.action(message)
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func action(message: String) {
+        print(message)
     }
-
-
+    
 }
 
+//
+//  Chained.swift
+//  链式编程
+//
+//  Created by houjianan on 16/7/22.
+//  Copyright © 2016年 houjianan. All rights reserved.
+//
+
+import Foundation
+
+class Chained: NSObject {
+    
+    typealias Handler = (message: String) -> ()
+    
+    func add(handler: Handler) -> Chained {
+        handler(message: "添加成功")
+        return self
+    }
+    
+    func show(handler: Handler) -> Chained {
+        handler(message: "显示成功")
+        return self
+    }
+    
+    func hide(handler: Handler) -> Chained {
+        handler(message: "隐藏成功")
+        return self
+    }
+    
+    func remove(isRemove: Bool, handler: Handler) -> Chained {
+        handler(message: "删除成功")
+        return self
+    }
+    
+}
